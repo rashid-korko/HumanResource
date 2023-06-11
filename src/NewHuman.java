8963ad854ac9ca7a1b1c99a9d77aefa8f0c9dd69
rashid-korko <benooo.sy.96@gmail.com>
2023-06-11 18:00:01 +0330

    second commit

--- src/NewHuman.java
+++ src/NewHuman.java
@@ -1,5 +1,6 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class NewHuman {
     void newhuman(){
@@ -11,11 +12,36 @@ public class NewHuman {
         String nationality;
         String ethnicities;
         String deathdate;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("name: ");
+        name = scanner.nextLine();
+        System.out.println("sex :");
+        sex = scanner.nextLine();
+        System.out.println("country :");
+        country = scanner.nextLine();
+        System.out.println("nationality :");
+        nationality = scanner.nextLine();
+        System.out.println("number :");
+        number = scanner.nextLine();
+        System.out.println("Birthday (dd/mm/yyyy) :");
+        birthdate = scanner.nextLine();
+        System.out.println("ethnicities :");
+        ethnicities = scanner.nextLine();
+        System.out.println("deathdate :");
+        deathdate = scanner.nextLine();
+        saveToFile( name ,  birthdate ,  sex ,  country ,  number ,  nationality ,  ethnicities ,  deathdate);
+        scanner.close();
     }
-    public void saveToFile() {
-        try (FileWriter writer = new FileWriter("HumanData.txt")) {
+    public void saveToFile(String name , String birthdate , String sex , String country , String number , String nationality , String ethnicities , String deathdate) {
+        try (FileWriter writer = new FileWriter("HumanData.txt" , true)) {
             writer.write("Name: " + name + "\n");
-            writer.write("Age: " + age + "\n");
+            writer.write("Birthdate: " + birthdate + "\n");
+            writer.write("Sex: " + sex + "\n");
+            writer.write("Country: " + country + "\n");
+            writer.write("Number: " + number + "\n");
+            writer.write("Nationality: " + nationality + "\n");
+            writer.write("Ethnicities: " + ethnicities + "\n");
+            writer.write("Deathdate: " + deathdate + "\n");
             System.out.println("Data saved to file.");
         } catch (IOException e) {
             e.printStackTrace();
