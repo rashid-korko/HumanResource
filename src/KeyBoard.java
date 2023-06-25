401dec62a6edd77db3509c5e7690b6027b19bc93
rashid-korko <benooo.sy.96@gmail.com>
2023-06-25 17:49:38 +0330

    6th commit

--- src/KeyBoard.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class KeyBoard {
-
-    public static String nextLine(){
-        Scanner keyboard = new Scanner(System.in);
-        return keyboard.nextLine();
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        FirstMenu firstMenu = new FirstMenu();
-        firstMenu.firstmenu();
+    public static void main(String[] args) {
+        // FirstMenu firstMenu = new FirstMenu();
+        // firstMenu.firstmenu();
+        Object object;
+        
     }
 }
